2caba4d47f8971e8072be95b7ea07cc50c38dc2b
cycaaa <cycaaa10@gmail.com>
2019-05-07 02:14:53 +0900

    sex

--- config/config.js
+++ config/config.js
@@ -9,14 +9,14 @@
  */
 
 var config = {
-	address: "172.16.98.14", // Address to listen on, can be:
+	address: "127.0.0.1", // Address to listen on, can be:
 	                      // - "localhost", "127.0.0.1", "::1" to listen on loopback interface
 	                      // - another specific IPv4/6 to listen on a specific interface
 	                      // - "", "0.0.0.0", "::" to listen on any interface
 	                      // Default, when address config is left out, is "localhost"
 	port: 9000,
 	
-    ipWhitelist: ["172.16.98.14", "::ffff:172.16.98.14", "::1","172.16.99.254", "::ffff:172.16.99.254", "::1"], // Set [] to allow all IP addresses
+    ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1","172.16.99.254", "::ffff:172.16.99.254", "::1"], // Set [] to allow all IP addresses
 	                                                       // or add a specific IPv4 of 192.168.1.5 :
 	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
 	                                                       // or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
